4d4ad54364094815c1e7a88c7630e5812d7240fe
Adit Ghosh <ghosh17@purdue.edu>
2017-08-22 15:52:34 -0700

    commit

---
+++ Source/asmFiled/subroutine_crc.asm
@@ -0,0 +1,38 @@
+#REGISTERS
+#at $1 at
+#v $2-3 function returns
+#a $4-7 function args
+#t $8-15 temps
+#s $16-23 saved temps (callee preserved)
+#t $24-25 temps
+#k $26-27 kernel
+#gp $28 gp (callee preserved)
+#sp $29 sp (callee preserved)
+#fp $30 fp (callee preserved)
+#ra $31 return address
+
+# USAGE random0 = crc(seed), random1 = crc(random0)
+#       randomN = crc(randomN-1)
+#------------------------------------------------------
+# $v0 = crc32($a0)
+crc32:
+  lui $t1, 0x04C1
+  ori $t1, $t1, 0x1DB7
+  or $t2, $0, $0
+  ori $t3, $0, 32
+
+l1:
+  slt $t4, $t2, $t3
+  beq $t4, $zero, l2
+
+  srl $t4, $a0, 31
+  sll $a0, $a0, 1
+  beq $t4, $0, l3
+  xor $a0, $a0, $t1
+l3:
+  addiu $t2, $t2, 1
+  j l1
+l2:
+  or $v0, $a0, $0
+  jr $ra
+#------------------------------------------------------
